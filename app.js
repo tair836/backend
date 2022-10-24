95b3b2ecf3c49a9e5c0d4aab163a2622bbd0be64
Eliav Menachi <eliav.menachi@gmail.com>
2022-10-24 13:53:37 +0300

    lecture 1 code

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv').config()
 const postRouter = require('./routes/post_route.js')
 app.use('/post',postRouter)
 
-
 app.listen(process.env.PORT,()=>{
     console.log('Server started')    
 })
